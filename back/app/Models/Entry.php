c3f5ef2773271e39f3894ab5b3f22483beb727c4
samsonin <samsonin@mail.ru>
2020-10-15 18:25:58 +1000

    add Entry

--- back/app/Models/Entry.php
+++ back/app/Models/Entry.php
@@ -5,11 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Publisher extends Model
+class Entry extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'name'
-    ];
+    protected $table = 'entries';
 }
---
+++ back/database/migrations/2020_10_15_020140_create_entries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->id();
+            $table->integer('publisher_id')->unsigned();
+            $table->integer('domain_id')->unsigned();
+            $table->string('name');
+            $table->boolean('is_app');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entries');
+    }
+}
