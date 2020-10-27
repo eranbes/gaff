b87171e080dc9da7b3bea4846c1af4606a8575bf
ivan <samsonin@mail.ru>
2020-10-27 23:02:41 +1000

    concat back and front

---
+++ back/public/precache-manifest.4317481a1dcac7024f5e6b2d7930a288.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975418277db24911e8314acad9099822",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e4ae28c56832a7c1378",
+    "url": "/static/js/2.06d12e7c.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/static/js/2.06d12e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2731c599d8fe6d477f1",
+    "url": "/static/js/main.13feeb5a.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  }
+]);
